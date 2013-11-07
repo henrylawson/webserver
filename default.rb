8c793b89db2e656f461c4166bf99fd4489748f72
Henry Lawson <henry.lawson@foinq.com>
2013-11-07 13:45:20 -0200

    Updated with the default cookbook generated by berkshelf

---
+++ default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (C) 2013 Henry Lawson
+# 
+# All rights reserved - Do Not Redistribute
+#
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'webserver'
+maintainer       'Henry Lawson'
+maintainer_email 'hen3rz@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures webserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
